39639dca13d9cc1b1c63f67195f06010997eb278
3l H4ck3r C0mf0r7 <pablexworld13@gmail.com>
2023-09-17 01:34:17 -0300

    Initial submodule commit

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Diagnostics;
+﻿using System;
+using System.Diagnostics;
 using System.ServiceProcess;
 
 namespace GetTrustedInstaller
@@ -7,7 +8,7 @@ namespace GetTrustedInstaller
     {
     
         
-        static void Main(string[] args)
+        static int Main(string[] args)
         {
 
             // I use TrustedInstaller process that is in the background only when the TrustedInstaller service is running to pass its ID to py7hagoras code
@@ -18,8 +19,21 @@ namespace GetTrustedInstaller
             if (sc.Status != ServiceControllerStatus.Running)
             sc.Start();
             Process[] proc = Process.GetProcessesByName("TrustedInstaller");
-            IamYourDaddy.Run(proc[0].Id, args[0]);
-           
+            string commandLine = string.Empty;
+            for (int i = 0; i < args.Length; i++)
+            {
+                if (i != 0) commandLine += " ";
+                commandLine += args[i];
+            }
+
+            Console.WriteLine($"Línea de comandos: {commandLine}");
+            Console.WriteLine(string.Empty);
+
+            if (commandLine != string.Empty) {
+                return IamYourDaddy.Run(proc[0].Id, commandLine, false);
+            } else {
+                return 3;
+            }
         }
 
 
